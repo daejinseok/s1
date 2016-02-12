922bc9a8e00e328f21da700471d3419a6d2700df
daejin <daejin.seok@gmail.com>
2016-02-12 20:46:57 +0900

    기본 단위 기능 완성

--- src/s1.js
+++ src/s1.js
@@ -31,6 +31,8 @@
         root.s1 = s1;
     }
 
+
+
     s1.randomID = function ( id_size ){
 
         var possible = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -86,29 +88,29 @@
      5. 문자, 숫자, 특수문자 조합
      */
 
-    // 1. 6개월간 동일하지 않은 패스워드
+    // 1. 6개월간 동일한 패스워드이면
     s1.include = function( pw, old_pw_list ){
-        return old_pw_list.indexOf(pw) === -1;
+        return old_pw_list.indexOf(pw) !== -1;
     };
 
     // 2. 개인정보 포함여부
     s1.deepInclude = function( pw, pinfo_list ){
 
         for (var idx in pinfo_list ) {
-            if ( pw.indexOf(pinfo_list[idx]) !== -1 ) return false;
+            if ( pw.indexOf(pinfo_list[idx]) !== -1 ) return true;
         }
 
-        return true;
+        return false;
     };
 
     // 숫자배열에서 연속된 숫자인지 확인
-    s1.isSeq = function( arr, parseFn, limit_size ){
+    var isSeq = function( arr, parseFn, limit_size ){
 
         // limit_size는 적어도 2이상이어야 함.
-        if ( limit_size < 2 ) return true;
+        if ( limit_size < 2 ) return false;
 
         // limit_size보다 작으면 ok
-        if ( arr.length < limit_size ) return true;
+        if ( arr.length < limit_size ) return false;
 
         var seq_size = 1;
         // idx 0은 시작시 사용함.
@@ -120,128 +122,83 @@
             if ( (num1+1) == num2 ) seq_size += 1;
             else                    seq_size  = 1;
 
-            if ( seq_size >= limit_size ) return false;
+            if ( seq_size >= limit_size ) return true;
         }
 
-        return true;
+        return false;
     };
 
-    s1.isNum3Seq = function( str_num ){
-        return s1.isSeq(str_num.split(''), function(s){return parseInt(s, 10)}, 3);
-    };
-
-    s1.abc2Idx = function( abc ){
+    var abc2Idx = function( abc ){
         return "abcdefghijklmnopqrstuvwxyz".indexOf(abc);
     };
 
+    s1.isNum3Seq = function( str_num ){
+        return isSeq(str_num.split(''), function(s){return parseInt(s, 10)}, 3);
+    };
+
     s1.isAbc3Seq = function( str_char ){
-        return s1.isSeq(str_char.toLowerCase().split(''), s1.abc2Idx, 3);
+        return isSeq(str_char.toLowerCase().split(''), abc2Idx, 3);
     };
 
 
 
+    var make_isIncludeSeq = function( regexp, isSeqFunction ){
+        return function( pw ){
+            var seqList = pw.match(regexp);
+
+            for(var idx in seqList){
+                if ( isSeqFunction(seqList[idx]) ) return true;
+            }
+
+            return false;
+        };
+    };
+
+    // pw에 3자리 이상 연속된 숫자가 포함되면 true
+    // pw에 3자리 이상 연속된 문자가 포함되면 true
+    s1.isInclude_num3Seq = make_isIncludeSeq(/\d\d+/g,             s1.isNum3Seq );
+    s1.isInclude_abc3Seq = make_isIncludeSeq(/[a-zA-Z][a-zA-Z]+/g, s1.isAbc3Seq );
 
 
     // 특수문자, 숫자, 문자가 포함여부를 확인하는 함수
-    function make_isIncludeChar( include_regexp ){
+    var make_isIncludeChar = function( include_regexp ){
         return function(pw){
             return pw.search(include_regexp) !== -1
         };
-    }
+    };
 
     s1.isIncludeSpecialChar = make_isIncludeChar(/[~!\#$^&*\=+|:;?"<,.>']/);
     s1.isIncludeNumberChar  = make_isIncludeChar(/[\d]/);
     s1.isIncludeAbcChar     = make_isIncludeChar(/[a-zA-Z]/);
 
 
+    /*
+     1. 6개월간 동일하지 않은 패스워드
+     2. 개인정보 포함여부
+     3. 사전 단어 포함여부
+     4. 3자리 이상 연속된 숫자 또는 문자 제한
+     5. 문자, 숫자, 특수문자 조합
+     */
 
-// 3. 사전은 "2"와 동일함.
+    s1.ERR_OLD_PW = 1;
+    s1.ERR_INCLIDE_PERSONAL = 2;
+    s1.ERR_INCLIDE_DICT = 3;
+    s1.ERR_NOT_INCLUDE_SPCHAR = 4;
+    s1.ERR_NOT_INCLUDE_NUMBER = 4;
 
+    s1.isGoodPW = function( pw, info ){
 
-// 4. 3자리 이상 연속된 숫자 또는 문자 제한
-    function is_way4( pw ){
-
-        var seq_size = 3;
-
-        var num_list = pw.match(/\d\d\d*/g);
-
-        for(var idx in num_list){
-            if ( !is_way4_1(num_list[idx], seq_size) ) return false;
+        if ( s1.include( pw, info.old_pw_list ) ) {
+            return s1.ERR_OLD_PW;
         }
 
-        return true;
-
-    }
-
-// 4.1 연속된 숫자인지 확인
-//
-//
-// word : 숫자문자열 ex 123
-// max_seq_size : 연속된는 길이
-
-
-// acd... 숫자 배열로 변경한다.
-    function abc_to_idx( w ){
-
-        var _map = "abcdefghijklmnopqrstuvwxyz";
-        var low = w.toLowerCase();
-
-        var rst = [];
-
-
-        for ( var idx in low ) {
-            rst.push(_map.indexOf(low[idx]));
+        if ( s1.deepInclude(pw, info.p_list) ){
+            return s1.ERR_INCLIDE_PERSONAL;
         }
 
-        return rst;
-    }
-
-// 4.1 연속된 숫자인지 확인
-//
-//
-// word : 숫자문자열 ex 123
-// max_seq_size : 연속된는 길이
-    function is_way4_2( word, seq_size ){
-
-        // max_seq_size는 적어도 2이상이어야 함.
-        if ( seq_size < 2 ) return true;
-
-        // max_seq_size보다 작으면 ok
-        if ( word.length < seq_size ) return true;
-
-        var cur_seq = 1;
-
-        var idx_list = abc_to_idx(word);
-
-        // idx 0은 시작시 사용함.
-        for( var idx = 1; idx < idx_list.length; idx++ ) {
-
-            var num1 = idx_list[idx-1];
-            var num2 = idx_list[idx];
-
-            if ( (num1+1) == num2 ) cur_seq += 1;
-            else                    cur_seq  = 1;
-
-            if ( cur_seq >= seq_size ) return false;
+        if ( s1.deepInclude(pw, info.dict) ){
+            return s1.ERR_INCLIDE_DICT;
         }
-
-        return true;
-    }
-
-
-// 특수문자, 숫자, 문자가 포함여부를 확인하는 함수
-    function make_is_include_char( include_regexp ){
-        return function(pw){
-            return pw.search(include_regexp) !== -1
-        };
-    }
-
-    var is_include_special_char = make_is_include_char(/[~!\#$^&*\=+|:;?"<,.>']/);
-    var is_include_number_char  = make_is_include_char(/[\d]/);
-    var is_include_abc_char     = make_is_include_char(/[a-zA-Z]/);
-
-
-// 5. 문자, 숫자, 특수문자 조합
-
+    };
 
 })();
--- test/s1Test.js
+++ test/s1Test.js
@@ -41,9 +41,9 @@ QUnit.test( "include", function( assert ) {
 
     var old_pw_list = ['daejin', 'hana'];
 
-    assert.notOk(s1.include("daejin", old_pw_list));
-    assert.notOk(s1.include("hana", old_pw_list));
-    assert.ok(s1.include("hana2", old_pw_list));
+    assert.ok(s1.include("daejin", old_pw_list));
+    assert.ok(s1.include("hana", old_pw_list));
+    assert.notOk(s1.include("hana2", old_pw_list));
 
 });
 
@@ -51,37 +51,138 @@ QUnit.test( "deepInclude", function( assert ) {
 
     var pinfo_list = ['daejin', 'hana'];
 
-    assert.notOk( s1.deepInclude('daejihana', pinfo_list));
-    assert.notOk( s1.deepInclude('hana123123', pinfo_list));
-    assert.ok( s1.deepInclude('daejha', pinfo_list));
+    assert.ok( s1.deepInclude('daejihana', pinfo_list));
+    assert.ok( s1.deepInclude('hana123123', pinfo_list));
+    assert.notOk( s1.deepInclude('daejha', pinfo_list));
 
 });
 
 QUnit.test( "isNum3Seq", function( assert ) {
-    assert.ok(s1.isNum3Seq("1"));
-    assert.ok(s1.isNum3Seq("12"));
-    assert.notOk(s1.isNum3Seq("123"));
-    assert.notOk(s1.isNum3Seq("1234"));
-    assert.ok(s1.isNum3Seq("1243"));
-    assert.notOk(s1.isNum3Seq("124345"));
-    assert.notOk(s1.isNum3Seq("124347345"));
-    assert.ok(s1.isNum3Seq("1243473145"));
-});
-
-QUnit.test( "abc2Idx", function( assert ) {
-    assert.equal(s1.abc2Idx("a"), 0);
-    assert.equal(s1.abc2Idx("d"), 3);
-    assert.equal(s1.abc2Idx("z"), 25);
+    assert.notOk(s1.isNum3Seq("1"));
+    assert.notOk(s1.isNum3Seq("12"));
+    assert.ok(s1.isNum3Seq("123"));
+    assert.ok(s1.isNum3Seq("1234"));
+    assert.notOk(s1.isNum3Seq("1243"));
+    assert.ok(s1.isNum3Seq("124345"));
+    assert.ok(s1.isNum3Seq("124347345"));
+    assert.notOk(s1.isNum3Seq("1243473145"));
 });
 
 QUnit.test( "isAbc3Seq", function( assert ) {
-    assert.ok(s1.isAbc3Seq("a"));
-    assert.ok(s1.isAbc3Seq("ab"));
-    assert.notOk(s1.isAbc3Seq("abc"));
-    assert.notOk(s1.isAbc3Seq("abcd"));
-    assert.ok(s1.isAbc3Seq("abdc"));
-    assert.notOk(s1.isAbc3Seq("abdcde"));
-    assert.notOk(s1.isAbc3Seq("abdcfcDe"));
-    assert.ok(s1.isAbc3Seq("abdcdgcaef"));
+    assert.notOk(s1.isAbc3Seq("a"));
+    assert.notOk(s1.isAbc3Seq("ab"));
+    assert.ok(s1.isAbc3Seq("abc"));
+    assert.ok(s1.isAbc3Seq("abcd"));
+    assert.notOk(s1.isAbc3Seq("abdc"));
+    assert.ok(s1.isAbc3Seq("abdcde"));
+    assert.ok(s1.isAbc3Seq("abdcfcDe"));
+    assert.notOk(s1.isAbc3Seq("abdcdgcaef"));
 });
 
+QUnit.test( "isInclude_num3Seq", function( assert ) {
+    assert.notOk(s1.isInclude_num3Seq("1"));
+    assert.notOk(s1.isInclude_num3Seq("12"));
+    assert.ok(s1.isInclude_num3Seq("123"));
+    assert.ok(s1.isInclude_num3Seq("1234"));
+    assert.notOk(s1.isInclude_num3Seq("1243"));
+    assert.ok(s1.isInclude_num3Seq("124345"));
+    assert.ok(s1.isInclude_num3Seq("124347345"));
+    assert.notOk(s1.isInclude_num3Seq("1243473145"));
+
+    assert.notOk(s1.isInclude_num3Seq("ab1"));
+    assert.notOk(s1.isInclude_num3Seq("12bc"));
+    assert.ok(s1.isInclude_num3Seq("ab123c"));
+    assert.ok(s1.isInclude_num3Seq("aaccc1234"));
+    assert.notOk(s1.isInclude_num3Seq("1243"));
+    assert.ok(s1.isInclude_num3Seq("abc12abc4aa345ccc"));
+    assert.ok(s1.isInclude_num3Seq("124347cccdd345aaaaa"));
+    assert.notOk(s1.isInclude_num3Seq("1243473145"));
+
+    assert.notOk(s1.isInclude_num3Seq("1"));
+    assert.notOk(s1.isInclude_num3Seq("1a2"));
+    assert.notOk(s1.isInclude_num3Seq("1a23"));
+    assert.notOk(s1.isInclude_num3Seq("12bc34"));
+    assert.notOk(s1.isInclude_num3Seq("12ad43"));
+    assert.notOk(s1.isInclude_num3Seq("1243aaaa45"));
+    assert.notOk(s1.isInclude_num3Seq("1243473aaa45"));
+    assert.notOk(s1.isInclude_num3Seq("1aaaa24347aaa3bb14bb5"));
+});
+
+QUnit.test( "isInclude_abc3Seq", function( assert ) {
+    assert.notOk(s1.isInclude_abc3Seq("a"));
+    assert.notOk(s1.isInclude_abc3Seq("ab"));
+    assert.ok(s1.isInclude_abc3Seq("abc"));
+    assert.ok(s1.isInclude_abc3Seq("abcd"));
+    assert.notOk(s1.isInclude_abc3Seq("abdc"));
+    assert.ok(s1.isInclude_abc3Seq("abdcde"));
+    assert.ok(s1.isInclude_abc3Seq("abdcfcDe"));
+    assert.notOk(s1.isInclude_abc3Seq("abdcdgcaef"));
+
+    assert.notOk(s1.isInclude_abc3Seq("a1231234"));
+    assert.notOk(s1.isInclude_abc3Seq("12341234ab"));
+    assert.ok(s1.isInclude_abc3Seq("1234123abc1341234"));
+    assert.ok(s1.isInclude_abc3Seq("123abc123d1234"));
+    assert.notOk(s1.isInclude_abc3Seq("1abdc1"));
+    assert.ok(s1.isInclude_abc3Seq("2abdcde2"));
+    assert.ok(s1.isInclude_abc3Seq("34abdcfcDe123"));
+    assert.notOk(s1.isInclude_abc3Seq("1234abdcdgcaef31234"));
+});
+
+
+
+
+
+
+
+QUnit.test( "isGoodPW", function( assert ) {
+
+
+    /*
+     1. 6개월간 동일하지 않은 패스워드
+     2. 개인정보 포함여부
+     3. 사전 단어 포함여부
+     4. 3자리 이상 연속된 숫자 또는 문자 제한
+     5. 문자, 숫자, 특수문자 조합
+     */
+
+    // 지난 패스워드
+    var old_password_list = [
+        'daejin',
+        'hana',
+        'hajin',
+        'jinwon'
+    ];
+
+    // 개인정보
+    var personal_list = [
+        '1234',
+        '1004',
+        'seok'
+    ];
+
+    // 사전단어
+    var word_list = [
+        'activity',
+        'admiral',
+        'adult',
+        'advantage',
+        'affair',
+        'amount',
+        'angel',
+        'background'
+    ];
+
+    var info = {
+        old_pw_list : old_password_list
+        , p_list    : personal_list
+        , dict      : word_list
+    };
+
+    assert.equal(s1.isGoodPW('daejin', info), s1.ERR_OLD_PW );
+    assert.equal(s1.isGoodPW('dae1234jin', info), s1.ERR_INCLIDE_PERSONAL );
+    assert.equal(s1.isGoodPW('daeangeljin', info), s1.ERR_INCLIDE_DICT );
+
+
+
+
+});
